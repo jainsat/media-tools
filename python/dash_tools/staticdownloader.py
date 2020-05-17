f03eced643e2164aa2f831e8c018bcd88249e19e
Ritesh Sinha <ritesh_sinha@ymail.com>
2020-05-17 02:27:57 +0000

    Addding segment sizes

--- python/dash_tools/staticdownloader.py
+++ python/dash_tools/staticdownloader.py
@@ -33,11 +33,8 @@ def download(options, mpd_url=None, mpd_str=None, base_url=None, base_dst=""):
         print("Starting BBA2 client")
         client.BBAClient(mpd_parser.mpd, base_url, base_dst, options, segment_sizes).download_bba2()
     elif options.pensieve:
-        with open('/home/rksinha_cs_stonybrook_edu/mpd_data.json') as json_file:
-            data = json.load(json_file)
-            segment_sizes = data['segment_size']
         print("Starting Pensieve client")
-        client.PensieveClient(mpd_parser.mpd, base_url, base_dst, options, segment_sizes).download_pensieve()
+        client.PensieveClient(mpd_parser.mpd, base_url, base_dst, options).download_pensieve()
     else:
         print("Starting Simple client")
         client.SimpleClient(mpd_parser.mpd, base_url, base_dst).download()
